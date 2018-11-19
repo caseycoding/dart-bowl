621a9814ef0c31851df41155f0c2bba85709d4de
casey billman <>
2018-11-18 20:23:04 -0600

    make schemas more simple

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,73 +1,15 @@
 # Schema definition, based on Cerberus grammar. Check the Cerberus project
 # (https://github.com/pyeve/cerberus) for details.
-game_schema = {
-    "lane": {
-        "type": "number",
-        "required": True,
-    },
-    "players": {
-        "type": "list",
-        "required": True,
-        "schema": {
-            "type": "objectid",
-        }
-    },
-    "scores": {
-        "type": "dict",
-        "required": False,
-        "keyschema": {
-            "type": "objectid",
-        },
-        "valueschema": {
-            "type": "dict",
-            "schema": {
-                "total_score": {
-                    "type": "number",
-                },
-                "player_id": {
-                    "type": "objectid",
-                },
-                "dispay_name": {
-                    "type": "string",
-                }, 
-                "frames": {
-                    "type": "list",
-                }
-            }
-        }
-    }
-}
-
-player_schema = {
-    "displayName": {
-        "type": "string",
-        "minlength": 1,
-        "maxlength": 10,
-        "required": True,
-    },
+create_game_schema = {
+    "lane": {"type": "number", "required": False},
+    "players": {"type": "list", "required": True, "schema": {"type": "objectid"}},
 }
 
 roll_schema = {
-    "game": {
-        "type": "objectid",
-        "required": True,
-        "data_relation": {
-            "resource": "games",
-            "field": "_id",
-                     "embeddable": True
-        },
-    },
-    "player": {
-        "type": "objectid",
-        "required": True,
-        "data_relation": {
-            "resource": "players",
-            "field": "_id",
-                     "embeddable": True
-        },
-    },
-    "pins": {
-        "type": "string", # string to handle strikes
-        "required": True,
-    }
+    "player_id": {"type": "objectid", "required": True},
+    "pins_knocked_down": {"type": "number", "required": True},
+}
+
+player_schema = {
+    "displayName": {"type": "string", "minlength": 1, "maxlength": 10, "required": True}
 }
--- backend/settings.py
+++ backend/settings.py
@@ -1,16 +1,9 @@
-import os
 import schemas
 
 DOMAIN = {
-    "players": {
-        "schema": schemas.player_schema
-    },
-    "games": {
-        "schema": schemas.game_schema
-    },
-    "rolls": {
-        "schema": schemas.roll_schema
-    }
+    "players": {"schema": schemas.player_schema},
+    "games": {"schema": schemas.create_game_schema},
+    "roll": {"schema": schemas.roll_schema, "url": "games/<regex('[a-f0-9]{24}'):game_id>/rolls"},
 }
 
 # database
@@ -22,4 +15,4 @@ RESOURCE_METHODS = ["GET", "POST", "DELETE"]
 ITEM_METHODS = ["GET", "PATCH", "PUT", "DELETE"]
 
 X_DOMAINS = ["localhost", "www.dartbowl.com"]
-
+BANDWIDTH_SAVER = False  # allows for the full object to be returned on creation
